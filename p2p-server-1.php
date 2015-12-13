bd0ae5c4c8b97501d814fbcdac94df27a685d7c9
Conor <conor@graphicandwebdesign.ie>
2015-12-13 11:22:34 +0000

    Sending JSON data

--- p2p-server-1.php
+++ p2p-server-1.php
@@ -8,11 +8,6 @@ require "lib/ip.functions.php";
 * Init
 */
 echo "\n>>> P2P Server 1 Reporting for duty\n\n";
-//127.0.0.1
-//1337
-
-
-
 
 //$my_ip_address = getHostByName(getHostName());// Not relianble for networking - gives VM Ethernet adapter, we need Wireless Lan adapter
 $my_ip_address = get_my_ip();
@@ -26,12 +21,29 @@ $port = 1337;
 // Socket resource
 $server = stream_socket_server("tcp://$my_ip_address:$port", $errno, $errorMessage);
 
-
-
-
 //echo "> My IP: " . $my_ip_address . PHP_EOL;
 echo "> My Stream Socket IP: " . $my_ip_address . PHP_EOL;
 echo "> My Stream Socket Port: " . $port . PHP_EOL;
+
+$peer = "192.168.75.187";
+
+
+
+/*
+* SAve my network data
+*/
+$my_network_data = array(
+    'my_ip_address'=> $my_ip_address, 
+    'port'=> $port);
+
+
+$fp = fopen('network/my-data.json', 'w');
+fwrite($fp, json_encode($my_network_data));
+fclose($fp);
+
+$my_message = "";
+
+$my_message = serialize(json_encode($my_network_data));
 
 /*
 * Code
@@ -44,7 +56,22 @@ if ($server === false) {
 
 }
 
+$counter_1 = 0;
+
 for (;;) {
+
+    if($counter_1 === 0){
+
+        echo "> Counter is 0: " . PHP_EOL;
+        $counter_1++;
+
+        exec("curl --data $my_message 192.168.75.187:1337");
+
+        continue;
+
+    } 
+
+    
 
     echo "> Listening for Client connection..." . PHP_EOL;
 
@@ -55,6 +82,7 @@ for (;;) {
 	echo "> Message Received from Client" . PHP_EOL;
 	$sock_data = fread($client, 1024);
 	echo $sock_data . PHP_EOL;
+    //echo json_decode($sock_data) . PHP_EOL;
 	echo "> Message Complete" . PHP_EOL;
 	//echo fwrite($client, "Hello! The time is ".date("n/j/Y g:i a")."\n");
         //echo stream_copy_to_stream($client, $client) . PHP_EOL;
@@ -63,6 +91,16 @@ for (;;) {
 
         fclose($client);
     }
+
+    
+
+
+
+    echo "> Sending message to peer $peer" . PHP_EOL;
+
+    //exec('curl --data "Hi from Win 8" $peer:1337');
+
+    exec("curl --data $my_message 192.168.75.187:1337");
 }
 
 
